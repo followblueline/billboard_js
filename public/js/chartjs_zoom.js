4e52924f512b89116afd97c990be62c1e392ec36
Zoran Bošnjak <zoran.bosnjak@gmail.com>
2021-11-10 14:32:59 +0100

    chartjs zoom working

--- public/js/chartjs_zoom.js
+++ public/js/chartjs_zoom.js
@@ -1,4 +1,8 @@
 //import Chart from 'chart.js/auto';
+let chart1 = null;
+let self = this;
+//import {CrosshairPlugin,Interpolate} from '/libs/chartjs-plugin-crosshair.js'
+//Chart.register(CrosshairPlugin);
 
 var colors = ["#e6194b", "#3cb44b", "#42d4f4", "#911eb4", "#f58231", "#bfef45", "#4363d8", "#dcbeff", "#006400", "#800000", "#fabed4", "#ff69b4"];
 var ctx1 = document.getElementById('chart1');
@@ -46,66 +50,96 @@ let chartData = {
         xAxisKey: 'time',
         yAxisKey: 'count'
       }
-    }
-              ]
-  };
+    }]
+};
   
-  let config = {
+let config = {
     type: 'bar',
     data: chartData,
     options: {
-      interaction: {
-        mode: 'x'
-      },
-      plugins: {
-        title: {
-          display: true,
-          text: 'Requests distribution'
+        interaction: {
+            mode: 'x'
         },
-        tooltip: {
-          callbacks: {
-            title: tooltipItems => {
-              return tooltipItems[0].raw.time || '';
+        plugins: {
+            title: {
+                display: true,
+                text: 'Requests distribution'
             },
-          }
-        }
-      },
-      responsive: true,
-      scales: {
-        x: {
-          stacked: true,
-          type: 'time',
-          time: {
-            unit: 'day'
-          },
-          min: '2021-07-01T00:00:00Z',
-          max: '2021-10-31T00:00:00Z'
-        },
-        y: {
-          stacked: true,
-          beginAtZero: true,
-          grid:{
-            display:true
-          },
-          ticks: {
-            callback: function(value, index, values) {
-              // only integers
-              if (value % 1 === 0)
-                return value;
+            tooltip: {
+                callbacks: {
+                    title: tooltipItems => {
+                        return tooltipItems[0].raw.time || '';
+                    },
+                }
             }
-          }
         },
-      }
-    }
-  };
-  
-  var chart1 = new Chart(ctx1, config);
-  
-  function dateToISOdateString(date) {
-    if (!date){
-      return null;
-    }
-    return date.getFullYear() +'-'+ (date.getMonth() +1).toString().padStart(2,'0') +'-'+ date.getDate().toString().padStart(2,'0');
-  }
+        zoom: {
+            enabled: true,                                      // enable zooming
+            zoomboxBackgroundColor: 'rgba(66,133,244,0.2)',     // background color of zoom box 
+            zoomboxBorderColor: '#48F',                         // border color of zoom box
+            zoomButtonText: 'Reset Zoom',                       // reset zoom button text
+            zoomButtonClass: 'reset-zoom',                      // reset zoom button class
+        },
+        crosshair: {
+            sync: {
+                enabled: false,
+                group: 1
+            },
+            callbacks: {
+                beforeZoom: () => function(start, end) {                  
+                // called before zoom, return false to prevent zoom
+                //  console.log('beforeZoom',start, end, self.chart.data);
+                return true;
+                },
+                afterZoom: (x) => function(start, end) {
+                    console.log('afterZoom', start, end, x)
+                    setTimeout(function() {
+                        // https://chartjs-plugin-crosshair.netlify.app/
+                        // https://chartjs-plugin-crosshair.netlify.app/samples/
+                        chart1.data.datasets[0].data = blacklist.filter(x => x.epoch >= start && x.epoch <= end);
+                        chart1.data.datasets[1].data = whitelist.filter(x => x.epoch >= start && x.epoch <= end);
+                        chart1.data.datasets[2].data = none.filter(x => x.epoch >= start && x.epoch <= end);
+                        chart1.update();
+                    }, 200)
+                }
+            }
+        },
+        responsive: true,
+        scales: {
+            x: {
+                stacked: true,
+                type: 'time',
+                time: {
+                unit: 'day'
+                },
+                min: '2021-07-01T00:00:00Z',
+                max: '2021-10-31T00:00:00Z'
+            },
+            y: {
+                stacked: true,
+                beginAtZero: true,
+                grid:{
+                    display:true
+                },
+                ticks: {
+                    callback: function(value, index, values) {
+                        // only integers
+                        if (value % 1 === 0)
+                        return value;
+                    }
+                }
+            },
+        }
+    }   
+};
 
-  export default { chart1 }
+chart1 = new Chart(ctx1, config);
+
+function dateToISOdateString(date) {
+if (!date){
+    return null;
+}
+return date.getFullYear() +'-'+ (date.getMonth() +1).toString().padStart(2,'0') +'-'+ date.getDate().toString().padStart(2,'0');
+}
+
+export default { chart1 }
